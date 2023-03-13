e4db9b127f445d53c2f83ceebeff77a7058f5078
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-14 00:49:12 +0600

    power: ti: Disable general-regs-only on bq2579x drivers
    This is safe to do as the floating point operations are small and
    bound under the kernel neon guards to avoid register corruption.
    
    kvsnr113:
    revert previous old change of removing floating point
    
    Change-Id: I5f53500bc0db889d944a290617c87135108a69ed
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/power/supply/ti/bq2597x_charger_munch.c
+++ drivers/power/supply/ti/bq2597x_charger_munch.c
@@ -34,6 +34,7 @@
 #include <linux/debugfs.h>
 #include <linux/bitops.h>
 #include <linux/math64.h>
+#include <asm/neon.h>
 #include "bq25970_reg.h"
 /*#include "bq2597x.h"*/
 
@@ -56,7 +57,7 @@ enum {
 	ADC_MAX_NUM,
 };
 
-static int sc8551_adc_lsb[] = {
+static float sc8551_adc_lsb[] = {
 	[ADC_IBUS]	= SC8551_IBUS_ADC_LSB,
 	[ADC_VBUS]	= SC8551_VBUS_ADC_LSB,
 	[ADC_VAC]	= SC8551_VAC_ADC_LSB,
@@ -1093,7 +1094,9 @@ static int bq2597x_get_adc_data(struct bq2597x *bq, int channel,  int *result)
 		*result = t;
 
 		if (bq->chip_vendor == SC8551) {
-			*result = (u64)t * (u64)sc8551_adc_lsb[channel] / 10000000;
+			kernel_neon_begin();
+			*result = (int)(t * sc8551_adc_lsb[channel]);
+			kernel_neon_end();
 		}
 	}
 
